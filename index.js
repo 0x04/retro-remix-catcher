3800e94db371101844c15dd7f341628f7bbec1fd
0x04 <ok@0x04.de>
2020-07-11 19:19:44 +0200

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,155 @@
+#!/usr/bin/env node
+
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
+const program = require('commander');
+const info = require('./package');
+
+
+program
+  .version(info.version, '-v, --version')
+  .usage('[options] <output-file>')
+  .requiredOption('-h, --host <host>', 'host without protocol')
+  .requiredOption('-p, --path <path>', 'path - `${page}` is the placeholder of current page')
+  .option('-s, --start <n>', 'start page', '1')
+  .option('-e, --end <n>', 'end page')
+  .option('-x, --exclude <exclude>', 'regex to exclude specific links')
+  .option('--ssl', 'use ssl', true)
+  .option('--no-ssl', 'don\'t use ssl')
+  .parse(process.argv);
+
+const defaults = {
+  host: program.host,
+  path: program.path
+};
+
+/**
+ * Search for MP3s in `href` attributes.
+ * @param {string} content
+ * @return {string[]}
+ */
+function findMP3Links(content)
+{
+  let result = [];
+  let regExp = /href="([^"]+\.mp3)"/gi;
+  let matches = null;
+  let exclude = (program.exclude)
+    ? new RegExp(program.exclude)
+    : null;
+
+  while ((matches = regExp.exec(content)))
+  {
+    let match = decodeURIComponent(matches[1]);
+
+    if (exclude && exclude.test(match))
+    {
+      continue;
+    }
+
+    result.push(match);
+  }
+
+  return result;
+}
+
+/**
+ * Request content and search for MP3s.
+ * @param {number} number
+ * @return {Promise<string[]>}
+ */
+function getMP3OfPage(number)
+{
+  return new Promise((resolve, reject) =>
+  {
+    const options = {
+      ...defaults,
+      path: defaults.path.replace(/\${page}/g, number)
+    };
+
+    const client = (program.ssl) ? https : http;
+
+    client.get(options, (response) =>
+    {
+      if (response.statusCode !== 200)
+      {
+        console.error(`error: http status code \`${response.statusCode}\``);
+        process.exit(1);
+      }
+
+      let content = '';
+
+      response.on('data', chunk => content += chunk);
+      response.on('end', () => resolve(findMP3Links(content)));
+      response.on('error', error => reject(error));
+    });
+  });
+}
+
+/**
+ * Get all MP3s of given pages.
+ * @param {number} pageStart
+ * @param {number} pageEnd
+ * @return {Promise<string[]>}
+ */
+async function getMP3OfPages(pageStart, pageEnd = pageStart)
+{
+  let result = [];
+
+  for (let i = pageStart; i <= pageEnd; i++)
+  {
+    result = result.concat(await getMP3OfPage(i));
+  }
+
+  return result;
+}
+
+/**
+ * Create a M3U file from the given link array.
+ * @param {string[]} links
+ * @return {string}
+ */
+function createM3U(links)
+{
+  let result = '#EXTM3U\n';
+
+  links.forEach(track =>
+  {
+    let title = track.slice(track.lastIndexOf('/') + 1, -4);
+    let url = `http${(program.ssl) ? 's' : ''}://`
+      + defaults.host
+      + (/\/$/.test(track) ? '' : '/')
+      + track.replace(/ /g, '%20');
+
+    result += `#EXTINF:-1,${title}\n#EXTVLCOPT:network-caching=1000\n${url}\n`;
+  });
+
+  return result;
+}
+
+
+getMP3OfPages(parseInt(program.start), parseInt(program.end || program.start))
+  .then(
+    result =>
+    {
+      const m3u = createM3U(result);
+
+      if (program.args.length === 1)
+      {
+        const filename = program.args[0];
+
+        fs.writeFile(filename, m3u, (error) =>
+        {
+          if (error)
+          {
+            console.error(`error: can\'t write file "${error.message}"`);
+            process.exit(1);
+          }
+
+          console.log(`File '${filename}' with ${result.length} links written.`);
+        });
+      }
+      else console.log(m3u);
+    }
+  );
---
+++ readme.md
@@ -0,0 +1,95 @@
+# retro-remix-catcher
+
+A tiny [CLI][CLI] script for collecting audio file links from retro remix
+websites and store them as [M3U][M3U] playlist file.
+
+
+## Installation
+
+Using NPM:
+
+```
+$ npm install --global @0x04/retro-remix-catcher
+```
+
+
+## Usage
+
+```
+$ retro-remix-catcher --help
+
+Usage: retro-remix-catcher [options] <output-file>
+
+Options:
+  -v, --version            output the version number
+  -h, --host <host>        host without protocol
+  -p, --path <path>        path - `${page}` is the placeholder of current page
+  -s, --start <n>          start page (default: "1")
+  -e, --end <n>            end page
+  -x, --exclude <exclude>  regex to exclude specific links
+  --ssl                    use ssl (default: true)
+  --no-ssl                 don't use ssl
+  --help                   display help for command
+```
+
+
+## Support sites
+
+This script is working with:
+
+* [amigaremix.com][AmigaRemix] 
+* [remix.kwed.org][RKO] 
+
+Other sites may also work.
+
+
+### Examples
+
+#### [amigaremix.com][AmigaRemix]
+
+Get pages from `1` to `32` and save it as "[atari.remix.org.m3u][ExampleM3U_AmigaRemix]".
+
+```
+$ retro-remix-catcher -h amigaremix.com -p '/remixes/${page}/' -s 1 -e 32 amigaremix.com.m3u
+```
+
+#### [remix.kwed.org][RKO]
+
+Get pages from `1` to `15`, exclude every filename that contains "(extract)" and save it as "[remix.kwed.org.m3u][ExampleM3U_RKO]".  
+
+```
+$ retro-remix-catcher -h remix.kwed.org -p '/?page=${page}' -s 1 -e 15 -x '\(extract\)' remix.kwed.org.m3u
+```
+
+#### Example M3U files
+
+Generated M3U files from the usage examples above:
+
+* [atari.remix.org.m3u][ExampleM3U_AmigaRemix]
+* [remix.kwed.org.m3u][ExampleM3U_RKO]
+
+
+## Acknowledgements
+
+* Baum Inventions for the initial idea
+* AmigaRemix Crew for their awesome site [amigaremix.com][AmigaRemix]
+* RKO Crew for their awesome site [remix.kwed.org][RKO]
+* Every artist/creator of these wonderful pieces of music
+
+
+## License
+
+[MIT][license]
+
+
+<!-- Links -->
+[license]: ./license.md
+[RKO]: https://remix.kwed.org
+[AmigaRemix]: https://amigaremix.com
+[ExampleM3U_AmigaRemix]: ./example-files/amigaremix.com.m3u
+[ExampleM3U_RKO]: ./example-files/remix.kwed.org.m3u
+
+
+<!-- Appendix -->
+[CLI]: https://en.wikipedia.org/wiki/Command-line_interface
+[M3U]: https://en.wikipedia.org/wiki/M3U
